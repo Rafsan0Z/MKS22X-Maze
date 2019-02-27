c0594f6460b51b9687a6986eb099d247ad92025a
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-02-27 10:28:34 -0500

    private fields added!

--- Maze.java
+++ Maze.java
@@ -3,15 +3,17 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Maze{
 
+  private char[][]maze;
+  private boolean animate;
+
+
   public static void main(String args[]){
 
           String[][] ary;
-          //instead of a try/catch, you can throw the FileNotFoundException.
-          //This is generally bad behavior
 
           File text = new File("Maze1.txt");
           // can be a path like: "/full/path/to/file.txt" or "../data/file.txt"
-          String maze = "";
+          String Maze = "";
           int counter = 0;
           int width = 0;
           int length = 0;
@@ -21,7 +23,7 @@ public class Maze{
           while(inf.hasNextLine()){
               String line = inf.nextLine();
               if(width == 0){width = line.length();}
-              maze += line + "\n";
+              Maze += line + "\n";
               counter++;
           }
           length = counter;
@@ -29,6 +31,6 @@ public class Maze{
           for(int i = 0,j = 0; i < length && j < width; i = j + 1, j++){
             ary[i][j] = "";
           }
-          System.out.println(maze);
+          System.out.println(Maze);
       }
 }
